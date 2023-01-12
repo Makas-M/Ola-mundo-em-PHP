e00d7020b90867bbf5588b6a9321a54a34156410
Makas_M <antoniomacaringue27@gmail.com>
2023-01-12 16:54:57 +0200

    Ola Mundo

---
+++ olamundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+	<meta charset="utf-8">
+	<title>Ola Mundo em PHP</title>
+	<style>
+		h2 {
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<h1>Primeiro PHP</h1>
+	<?php
+		echo "<h2> Olá Mundo! </h2>";
+		?>
+
+</body>
+</html>
